678ddf6395ece5cc251163e4eef64ab6eb9e325c
tdv14011984@gmail.com <Parol2022!-forGIT>
2022-05-13 17:08:27 +0300

    add new test

---
+++ src/test/java/denis/timushev/searchNewTest.java
@@ -0,0 +1,25 @@
+package denis.timushev;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class searchNewTest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void searchYandexTest() {
+        open("https://yandex.ru/");
+        $(".home-link_hover_inherit").click();
+        $(".passp-exp-register-button").click();
+        $(".Link_weight_medium").shouldHave(text("Уже есть аккаунт"));
+    }
+}
